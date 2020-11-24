7485a290c3d471896f3609f352a9479469d9b15d
Nuttachai20 <teaynekd@gmail.com>
2020-11-24 14:47:08 +0700

    update0.6

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var edit = require('./routes/frontend/edit');
 var change = require('./routes/frontend/changepassword');
 var track = require('./routes/frontend/track');
 var detail = require('./routes/frontend/detail');
-  
+
 /**
  * backend 
  */
@@ -54,26 +54,26 @@ app.use('/changepassword', change);
 app.use('/tracks', track);
 app.use('/detail', detail);
 
-     /**
-      * backend route
-      */
+/**
+ * backend route
+ */
 app.use(trackRouter);
 app.use(userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
